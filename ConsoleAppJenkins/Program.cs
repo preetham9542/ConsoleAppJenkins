2c174fe9e7b3b64b0e18116e98058edb74dc296c
abc <abc@gmail.com>
2024-02-16 09:41:32 +0530

    Add project files.

---
+++ ConsoleAppJenkins/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppJenkins;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("*****Even Numbers*****");
+NumberSystem.EvenNum();
+Console.WriteLine("*****Odd Numbers");
+NumberSystem.OddNum();
